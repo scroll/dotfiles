08bf8fbf8b4a7a37cc253fbd21fdff8b56c66c87
Marin Petrov <petrov.marin@gmail.com>
2020-07-08 15:46:20 +0300

    Delete emacs.d

--- emacs.d/elpa/pylint-20170402.1255/pylint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pylint" "20170402.1255" "minor mode for running `pylint'" 'nil :commit "ac5b1a434a8dddf3cfea361eb5c65669e76dc9b1" :keywords '("languages" "python") :authors '(("Ian Eure" . "ian.eure@gmail.com")) :maintainer '("Jonathan Kotta" . "jpkotta@gmail.com"))
